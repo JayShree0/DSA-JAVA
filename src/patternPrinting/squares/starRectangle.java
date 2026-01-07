097ca83422153aa19a3c48f7ec9282dbb1c122fa
JAY SHREE PATEL <jayshree0>
2026-01-07 13:24:27 +0530

    Initial commit - DSA Java

---
+++ src/patternPrinting/squares/starRectangle.java
@@ -0,0 +1,10 @@
+package patternPrinting.squares;
+
+public class starRectangle {
+    public static void main(String[] args) {
+        System.out.println("*****");
+        System.out.println("*****");
+        System.out.println("*****");
+        System.out.println("*****");
+    }
+}
